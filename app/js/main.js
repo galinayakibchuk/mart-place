50d6a7254ea14e3f9a0faeafdcb3c3b5d86690b4
galinayakibchuk <galina.yakibchuk@gmail.com>
2020-07-01 18:26:50 +0530

    added index added main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,21 @@
 $(function(){
 
+    $('.slider__inner').slick({
+        prevArrows: '<button class="slick-prev slick-arrow lnr lnr-chevron-right></button>',
+        nextArrows: '<button class="slick-next slick-arrow lnr lnr-chevron-left"></button>',
+        infinite: true
+        
+    });
 
-
+    $('.followers__items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false
+      });
+          
+    
+    var mixer = mixitup('.newest__items');
 
 
     
